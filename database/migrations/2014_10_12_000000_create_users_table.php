20bb7bbcb5ea5a5473b14eefe7613f4cda87aff8
Miguel N <migueljfnogueira@gmail.com>
2020-04-29 22:36:38 +0100

    Ammend to previous commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid'); // Mojang UUID
+            $table->string('uuid')->nullable(); // Mojang UUID
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
