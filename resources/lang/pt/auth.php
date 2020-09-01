910863bdea383c8f2a0cbef43680833f512cfc30
Miguel N <miguel456@spacejewel-hosting.com>
2020-09-01 23:13:13 +0100

    New translations auth.php (Portuguese)

--- resources/lang/pt/auth.php
+++ resources/lang/pt/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Estas credenciais não coincidem com os nossos registos.',
+    'throttle' => 'Demasiadas tentativas de acesso. Tente novamente em :seconds segundos.',
 
 ];
