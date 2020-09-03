2ad1548cd6d45ff497e4bcb8b418db35114da231
Miguel N <miguel456@spacejewel-hosting.com>
2020-09-03 16:17:21 +0100

    New translations menu.php (Portuguese)

---
+++ resources/lang/vendor/adminlte/pt/menu.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'm_home' => 'Home',
+    'm_directory' => 'Directory',
+    'h_applications' => 'Applications',
+    'm_my_applications' => 'My Applications',
+    'm_curr_applications' => 'Current Applications',
+    'h_my_profile' => 'My Profile',
+    'm_profile_settings' => 'Profile Settings',
+    'm_account_settings' => 'My Account Settings',
+    'h_app_management' => 'Application Management',
+    'm_all_apps' => 'All Applications',
+    'm_outstanding_apps' => 'Outstanding Applications',
+    'm_interview_queue' => 'Interview Queue',
+    'm_peer_approval' => 'Peer Approval Queue',
+    'h_admin' => 'Administration',
+    'm_staff_m' => 'Staff Members',
+    'm_reg_players' => 'Registered Players',
+    'sm_hiring_man' => 'Hiring Management',
+    'm_open_pos' => 'Open Positions',
+    'sm_forms' => 'Forms',
+    'sm_all_forms' => 'All Forms',
+    'm_form_builder' => 'Form Builder',
+    'sm_app_settings' => 'App Settings',
+    'm_global_app_s' => 'Global App Settings',
+    'm_devtools' => 'Developer Tools',
+    'm_s_logs' => 'System Logs'
+
+
+];
