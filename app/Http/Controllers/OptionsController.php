bea83b650c29536cc5d18ae39753abb31ce22b27
Miguel N <miguel456@spacejewel-hosting.com>
2020-09-07 22:54:20 +0100

    Added more debug logging

--- app/Http/Controllers/OptionsController.php
+++ app/Http/Controllers/OptionsController.php
@@ -7,6 +7,7 @@ use App\Options as Option;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class OptionsController extends Controller
 {
@@ -29,17 +30,33 @@ class OptionsController extends Controller
    {
        if (Auth::user()->can('admin.settings.edit'))
        {
+           Log::debug('Updating application options', [
+               'ip' => $request->ip(),
+               'ua' => $request->userAgent(),
+               'username' => Auth::user()->name
+           ]);
            foreach($request->all() as $optionName => $option)
            {
                try
                {
-                   if (Options::optionExists($option))
+                   Log::debug('Going through option ' . $optionName);
+                   if (Options::optionExists($optionName))
                    {
+                       Log::debug('Option exists, updating to new values', [
+                           'opt' => $optionName,
+                           'new_value' => $option
+                       ]);
                        Options::changeOption($optionName, $option);
                    }
                }
                catch(\Exception $ex)
                {
+                   Log::error('Unable to update options!', [
+                       'msg' => $ex->getMessage(),
+                       'trace' => $ex->getTraceAsString()
+                   ]);
+                   report($ex);
+
                    $errorCond = true;
                    $request->session()->flash('error', 'An error occurred while trying to save settings: ' . $ex->getMessage());
                }
