982854d5c643ba2f16dbaf9e387ab56c9383624f
Miguel N <miguel456@spacejewel-hosting.com>
2020-09-10 22:47:51 +0100

    RSM-25 add relevant permisisons to teams

---
+++ database/seeds/TeamSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create([
+            'name' => 'teams.admin.create',
+        ]);
+        Permission::create([
+            'name' => 'teams.admin.delete',
+        ]);
+        Permission::create([
+            'name' => 'teams.user.join',
+        ]);
+        Permission::create([
+            'name' => 'teams.user.leave',
+        ]);
+        Permission::create([
+            'name' => 'teams.admin.vacancies.assign',
+        ]);
+        Permission::create([
+            'name' => 'teams.admin.vacancies.unassign',
+        ]);
+        Permission::create([
+            'name' => 'teams.admin.applications.changeteam',
+        ]);
+        Permission::create([
+            'name' => 'teams.members.appointment.schedule',
+        ]);
+        Permission::create([
+            'name' => 'teams.members.appointment.deleteappointment',
+        ]);
+        Permission::create([
+            'name' => 'teams.members.groupchat',
+        ]);
+
+        Permission::create([
+            'name' => 'chat.use',
+        ]);
+    }
+}
