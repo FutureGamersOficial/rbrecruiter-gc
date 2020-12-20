79571d8b4c69730800fdabfb542ace6cb431afd8
Proximity <me@proximity.sh>
2020-12-20 12:14:27 -0500

    Fixed variable caps

--- resources/views/breadcrumbs/footer.blade.php
+++ resources/views/breadcrumbs/footer.blade.php
@@ -3,7 +3,7 @@
 
     <!-- Copyright - DO NOT REMOVE WITHOUT PERMISSION -->
     <div class="footer-copyright text-center py-3">
-        <a href="{{ config('APP.URL') }}"> {{ config('APP.NAME') }} &copy; 2019-{{ now()->year }} - {{__('messages.footer_copy')}}</a>
+        <a href="{{ config('app.url') }}"> {{ config('app.name') }} &copy; 2019-{{ now()->year }} - {{__('messages.footer_copy')}}</a>
     </div>
     <!-- Copyright -->
     <!-- GNU General Public License (https://www.gnu.org/licenses/gpl-3.0.en.html) Built by Miguel Nogueira -->
