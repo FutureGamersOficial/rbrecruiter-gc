a72abb91475a519f63b33142bd60c7b7931cfa0c
Proximity <me@proximity.sh>
2020-12-20 12:11:25 -0500

    Updated footer variables

--- resources/views/breadcrumbs/footer.blade.php
+++ resources/views/breadcrumbs/footer.blade.php
@@ -3,7 +3,7 @@
 
     <!-- Copyright - DO NOT REMOVE WITHOUT PERMISSION -->
     <div class="footer-copyright text-center py-3">
-        <a href="{{ env('APP_URL') }}"> {{ env('APP_NAME') }} &copy; 2019-{{ now()->year }} - {{__('messages.footer_copy')}}</a>
+        <a href="{{ config('APP_URL') }}"> {{ config('APP_NAME') }} &copy; 2019-{{ now()->year }} - {{__('messages.footer_copy')}}</a>
     </div>
     <!-- Copyright -->
     <!-- GNU General Public License (https://www.gnu.org/licenses/gpl-3.0.en.html) Built by Miguel Nogueira -->
