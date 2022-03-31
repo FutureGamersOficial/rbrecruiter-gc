df6aa1e9298c731cfe743c201d191c0cbaac77a2
miguel456 <me@nogueira.codes>
2022-03-31 15:05:48 +0100

    refactor: update localization files
    Note: This commit removes previous translations, which means the app will now be untranslated.
    
    Reasons for this include low translation quality, and the lack of a good quality assurance process in place.

---
+++ lang/pt/adminlte::adminlte.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'email' => '',
+    'password' => '',
+    'retype_password' => '',
+    'verify_check_your_email' => '',
+    'verify_email_sent' => '',
+    'verify_if_not_recieved' => '',
+    'verify_message' => '',
+    'verify_request_another' => '',
+];
