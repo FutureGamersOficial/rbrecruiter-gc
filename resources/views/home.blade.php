77a781ed20519cfbc3146d1f3bb05ca331e9236d
miguel456 <me@nogueira.codes>
2022-04-07 15:37:56 +0100

    feat: remove embedded twitch player

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,28 +43,6 @@
 
 	  <div class="container-fluid">
 
-        <div class="row mt-2 mb-4">
-
-            <div class="col">
-                <div id="twitch-embed"></div>
-
-                <script src="https://embed.twitch.tv/embed/v1.js"></script>
-
-                <script type="text/javascript">
-                new Twitch.Embed("twitch-embed", {
-                    width: '100%',
-                    height: 580,
-                    channel: "gamesclub_oficial",
-                    muted: true, // don't annoy chrome with autoplay w/audio
-                    theme: 'light',
-                    layout: 'video'
-                });
-                </script>
-            </div>
-
-        </div>
-
-
           @if ($demoActive)
               <div class="row">
                   <div class="col">
